68f5c2a4da7094693418b61f11109637e54f9660
Chris Sevilleja <chris@scotch.io>
2016-08-22 15:09:14 -0700

    23-form-validations

--- app/users/user-form.component.ts
+++ app/users/user-form.component.ts
@@ -14,16 +14,18 @@ import { User } from '../shared/models/user';
   template: `
     <form #form="ngForm">
 
-      {{ form.valid }}
-
-      <div class="form-group">
-        <input type="text" class="form-control" name="name" required
+      <div class="form-group" [ngClass]="{ 'has-error': name.invalid && name.touched }">
+        <input type="text" class="form-control" name="name" placeholder="Name" required
           [(ngModel)]="newUser.name" #name="ngModel">
+
+        <span class="help-block" *ngIf="name.invalid && name.touched">Name is required.</span>
       </div>
 
-      <div class="form-group">
-        <input type="text" class="form-control" name="username" required
+      <div class="form-group" [ngClass]="{ 'has-error': username.invalid && username.touched }">
+        <input type="text" class="form-control" name="username" placeholder="Username" required
           [(ngModel)]="newUser.username" #username="ngModel">
+
+        <span class="help-block" *ngIf="username.invalid && username.touched">Username is required.</span>
       </div>
 
       <button type="submit" class="btn btn-lg btn-block btn-primary" [disabled]="form.invalid">
